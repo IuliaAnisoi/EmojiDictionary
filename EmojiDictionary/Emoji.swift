64be8a1aaaf769951fd9aeeb6f64b382383ac77a
Anisoi Iulia Roxana <iulia@192-168-0-105.rdsnet.ro>
2021-03-30 22:15:13 +0300

    first commit

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Anisoi Iulia Roxana on 17.03.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: Character
+    var name: String
+    var description: String
+    var usage: String
+}
